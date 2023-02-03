418c66686f377c27f6ca019a12cacb0c63f7eda2
alfarestaat <fares.tris@gmail.com>
2023-02-03 11:02:18 +0700

    Testcommit

---
+++ soal1.js
@@ -0,0 +1,10 @@
+var prompt = require('prompt-sync')();
+let x = prompt("Masukkan angka: ");
+
+if (x < 0) {
+  console.log("Tidak bisa input bilangan negatif");
+} else if (x % 2 !== 0) {
+  console.log("Tidak bisa input bilangan ganjil");
+} else {
+  console.log("Hasil akar pangkat 2 dari ", x,"adalah",Math.sqrt(x));
+}
